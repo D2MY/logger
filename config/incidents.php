62730234a4eaf79bb575d2234d4ee3936179c9a3
D2MY <vania.klimovich@gmail.com>
2023-03-18 19:01:01 +0300

    save

--- config/incidents.php
+++ config/incidents.php
@@ -4,16 +4,16 @@ return [
 
     'channels' => [
 //        'low' => [
-//            'handler' => \D2my\Impersonate\Services\DefaultHandler::class
+//            'handler' => \D2my\Incidents\Services\DefaultHandler::class
 //        ],
 //        'medium' => [
-//            'handler' => \D2my\Impersonate\Services\DefaultHandler::class
+//            'handler' => \D2my\Incidents\Services\DefaultHandler::class
 //        ],
 //        'high' => [
-//            'handler' => \D2my\Impersonate\Services\DefaultHandler::class
+//            'handler' => \D2my\Incidents\Services\DefaultHandler::class
 //        ],
 //        'blocker' => [
-//            'handler' => \D2my\Impersonate\Services\DefaultHandler::class
+//            'handler' => \D2my\Incidents\Services\DefaultHandler::class
 //        ],
     ],
 ];
